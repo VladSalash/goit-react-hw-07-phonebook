3754080a80b6773b4885bec7ff85318858a07172
VladSalash <salashchenko99@gmail.com>
2022-12-10 21:14:37 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import ContactForm from './Contact/ContactForm/ContactForm';
 import ContactList from './Contact/ContactList/ContactList';
-import Filter from './Contact/Filter/Filter';
 
 const App = () => {
   return (
@@ -8,7 +7,6 @@ const App = () => {
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
-      {/* <Filter title="Find contact by name" /> */}
       <ContactList />
     </div>
   );
